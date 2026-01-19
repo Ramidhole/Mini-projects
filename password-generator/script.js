d180cd489df993ea890106a6099023723b1d3c37
Ramidhole <121009055+Ramidhole@users.noreply.github.com>
2026-01-19 15:26:42 +0530

    create the password on click button

--- password-generator/script.js
+++ password-generator/script.js
@@ -14,3 +14,44 @@ inputSlider.addEventListener("input",()=>{
     sliderValue.textContent = inputSlider.value;
 
 })
+
+
+genbtn.addEventListener('click',()=>{
+
+    passBox.value = genratePassword();
+
+
+})
+
+let lowerCase = "abcdefghijklmnopqrstuvwxyz";
+let upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let allNumbers = "1234567890" ;
+let symBols = "!@#$%^&*";
+
+
+
+function genratePassword(){
+    let genPassword ="";
+    let allChars = "";
+
+    allChars  += lowercase.checked ? lowerCase: "";
+    allChars  += uppercase.checked? upperCase:"";
+    allChars  += numbers.checked? allNumbers:"";
+    allChars  += symbols.checked?symBols:"";
+
+
+    if(allChars == ""|| allChars.length == 0){
+        return genPassword;
+    }
+
+    let i = 1;
+    while(i<=inputSlider.value)
+{
+    genPassword += allChars.charAt(Math.floor(Math.random()*allChars.length));
+
+    i++;
+}
+
+return genPassword;
+   
+}
