e6161d6eae4d8d43d0b85f0095852e0d00c431b5
Ramidhole <121009055+Ramidhole@users.noreply.github.com>
2026-01-24 21:40:36 +0530

    initial commit:add serabox element

---
+++ CheckWhetherNow/WeatherNow/src/SearchBox.jsx
@@ -0,0 +1,14 @@
+import TextField from "@mui/material/TextField"
+
+export default function SearchBox() {
+  return (
+    
+      <div>
+        <h1>Search for weather</h1>
+        <form >
+
+      <TextField label="City Name" variant="outlined" id="city" />
+        </form>
+    </div>
+  )
+}
