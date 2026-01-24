a167f4f1434f010eca8b56ff163d6c7947ffa89b
Ramidhole <121009055+Ramidhole@users.noreply.github.com>
2026-01-25 01:29:18 +0530

    add submit button

--- CheckWhetherNow/WeatherNow/src/SearchBox.jsx
+++ CheckWhetherNow/WeatherNow/src/SearchBox.jsx
@@ -1,14 +1,30 @@
 import TextField from "@mui/material/TextField"
+import Button from "@mui/material/Button"
+import "./SearchBox.css"
+import { useState } from "react"
 
 export default function SearchBox() {
-  return (
-    
-      <div>
-        <h1>Search for weather</h1>
-        <form >
 
-      <TextField label="City Name" variant="outlined" id="city" />
-        </form>
+  let[city,setCity]=useState("");
+
+  let handleChange =(evt)=>{
+    setCity(evt.target.value);
+  };
+
+  let handleSubmit=(evt)=>{
+    evt.preventDefault();
+    console.log(city);
+    setCity("");
+  };
+
+  return (
+    <div className="search">
+      <h1>Search for weather</h1>
+      <form onSubmit={handleSubmit}>
+        <TextField label="City Name" variant="outlined" id="city" required value={city} onChange={handleChange} />
+        <br /><br />
+        <Button variant="contained" >Search</Button>
+      </form>
     </div>
   )
 }
